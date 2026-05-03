17f1db6457828b75b89d1f504b0ae74bc56d28ab
Ivan Rios <ivan.rios1712@gmail.com>
2026-05-03 18:50:29 +0200

    Subida del principio del despliegue

--- despliegue-aws/variables.tf
+++ despliegue-aws/variables.tf
@@ -1,31 +1,20 @@
-variable "aws_region" {
-  description = "Región de AWS"
-  type        = string
-  default     = "us-east-1"
-}
-
-variable "instance_type" {
-  description = "Tipo de instancia EC2"
-  type        = string
-  default     = "t2.micro"
-}
-
 variable "key_name" {
-  description = "Nombre de la clave SSH para EC2"
-  type        = string
-  default     = "vockey"
+  type    = string
+  default = "vockey"
 }
 
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
 
-variable "project_name" {
-  description = "Nombre del proyecto para etiquetar recursos"
-  type        = string
-  default     = "techuniverse"
+variable "domain" {
+  type    = string
+  default = "techuniverse.com"
 }
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["099720109477"] # Canonical (propietario oficial de Ubuntu)
 
   filter {
     name   = "name"
@@ -34,30 +23,18 @@ data "aws_ami" "ubuntu" {
 
   filter {
     name   = "virtualization-type"
-    values = ["hvm"] # Hardware Virtual Machine (virtualización completa)
+    values = ["hvm"]
   }
+
+  owners = ["099720109477"] # Canonical
 }
 
-# VPC default
-data "aws_vpc" "default" {
-  default = true
+variable "Instance_Type" {
+    type = string
+    default = "t2.medium"
 }
 
-# Subnet default
-data "aws_subnets" "default" {
-  filter {
-    name   = "vpc-id"
-    values = [data.aws_vpc.default.id]
-  }
-}
-
-variable "git_repo_url" {
-  description = "URL del repositorio Git para clonar el código en el servidor"
+variable "db_root_password" {
   type        = string
-}
-
-variable "allowed_ssh_cidr" {
-  description = "CIDR block permitido para acceso SSH"
-  type        = string
-  default     = "0.0.0.0/0"
+  sensitive   = true
 }
