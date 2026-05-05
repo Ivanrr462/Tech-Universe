6d6d441c31310123424696598606e38db8d8269d
Ivan Rios <ivan.rios1712@gmail.com>
2026-05-05 14:52:06 +0200

    todo ready

--- techuniverse-api/scripts/after_install.sh
+++ techuniverse-api/scripts/after_install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+chown -R ubuntu:ubuntu /var/www/api
 cd /var/www/api
 composer install --no-dev --optimize-autoloader
 cp /var/www/.env.base .env
@@ -8,3 +9,5 @@ php artisan db:seed --force
 php artisan config:cache
 php artisan route:cache
 chown -R www-data:www-data /var/www/api
+chmod -R 775 /var/www/api/storage
+chmod -R 775 /var/www/api/bootstrap/cache
