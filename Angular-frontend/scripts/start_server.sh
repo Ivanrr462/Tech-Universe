5e2241686e2fbd0e385970f7dc36e61a7f094eff
Ivan Rios <ivan.rios1712@gmail.com>
2026-05-08 15:24:20 +0200

    again

--- Angular-frontend/scripts/start_server.sh
+++ Angular-frontend/scripts/start_server.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-chown -R www-data:www-data /var/www/frontend
-systemctl restart apache2
+set -e
+
+chown -R www-data:www-data /var/www/frontend || true
+
+systemctl restart apache2 || systemctl restart httpd || true
