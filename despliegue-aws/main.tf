c31f300c2a3f2019dd744b04ce2fb623be634b29
Ivan Rios <ivan.rios1712@gmail.com>
2026-05-08 19:15:28 +0200

    a reza pq funcione

--- despliegue-aws/main.tf
+++ despliegue-aws/main.tf
@@ -121,6 +121,14 @@ resource "aws_vpc_security_group_ingress_rule" "https_frontend" {
   cidr_ipv4         = "0.0.0.0/0"
 }
 
+resource "aws_vpc_security_group_ingress_rule" "https_backend" {
+  security_group_id            = aws_security_group.BackEnd.id
+  from_port                    = 443
+  to_port                      = 443
+  ip_protocol                  = "TCP"
+  referenced_security_group_id = aws_security_group.FrontEnd.id
+}
+
 // db
 resource "aws_vpc_security_group_ingress_rule" "mysql_db" {
   security_group_id            = aws_security_group.db.id
--- techuniverse-api/scripts/after_install.sh
+++ techuniverse-api/scripts/after_install.sh
@@ -3,39 +3,23 @@ set -e
 
 cd /var/www/api
 
-# =========================
-# PERMISOS (SIEMPRE AL INICIO DEL DEPLOY)
-# =========================
 chown -R www-data:www-data /var/www/api
-
 chmod -R 775 storage bootstrap/cache
 
-# =========================
-# ENV
-# =========================
 if [ ! -f .env ]; then
   cp /var/www/.env.base .env
 fi
 
 php artisan key:generate --force || true
 
-# =========================
-# LARAVEL (SIN npm, SIN composer si usas build en CI)
-# =========================
 php artisan migrate --force --no-interaction
 
-# ⚠️ seed SOLO si es necesario
-# php artisan db:seed --force --no-interaction
-
-# =========================
-# CACHE LIMPIO
-# =========================
 php artisan optimize:clear
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
-# =========================
-# RESTART
-# =========================
+# IMPORTANTE FILAMENT
+php artisan filament:assets || true
+
 systemctl restart apache2 || true
